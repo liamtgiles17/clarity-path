f8ad805b7b0b010a258b152c4054865cd7a10546
liamtgiles17 <liamthomasgiles1702@gmail.com>
2024-12-05 14:34:08 +0000

    setup to publish to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/clarity-path",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
