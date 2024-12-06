b953516c74fd78223e4a53023a3391688da154e0
liamtgiles17 <liamthomasgiles1702@gmail.com>
2024-12-06 12:17:14 +0000

    actually fixed images

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,7 +83,7 @@ export default function AboutPage() {
                         <h2 className="text-center text-2xl pb-4 xl:text-3xl xl:pb-8"><b>What is ADHD?</b></h2>
                         <div className="w-full h-full">
                             <div className="md:px-[10%] max-md:px-[5%]">
-                                <div className="bg-cover sm:hidden" style={{backgroundImage: `linear-gradient(to right, rgba(231, 231, 231, 1) 0%, rgba(231, 231, 231, 1) 30%, rgba(231, 231, 231, 0.3) 70%, rgba(231, 231, 231, 0.3) 80%, rgba(231, 231, 231, 1) 100%), linear-gradient(to bottom, rgba(231, 231, 231, 1) 0%, rgba(231, 231, 231, 0) 15%, rgba(231, 231, 231, 0) 85%, rgba(231, 231, 231, 1) 100%), url(/stock6small${isExtraSmall ? "2" : ""}.avif)`}}>
+                                <div className="bg-cover sm:hidden" style={{backgroundImage: `linear-gradient(to right, rgba(231, 231, 231, 1) 0%, rgba(231, 231, 231, 1) 30%, rgba(231, 231, 231, 0.3) 70%, rgba(231, 231, 231, 0.3) 80%, rgba(231, 231, 231, 1) 100%), linear-gradient(to bottom, rgba(231, 231, 231, 1) 0%, rgba(231, 231, 231, 0) 15%, rgba(231, 231, 231, 0) 85%, rgba(231, 231, 231, 1) 100%), url(/clarity-path/stock6small${isExtraSmall ? "2" : ""}.avif)`}}>
                                     <div className="max-md:pb-2 text-left mr-[40%]">
                                         <p className="pb-4 md:text-lg max-md:text-md xl:text-xl">
                                             ADHD, or <b>Attention-Deficit/Hyperactivity Disorder</b>, is a neurodevelopmental condition that affects a person&apos;s ability to focus, control impulses, and manage activity levels appropriately for a given situation.
@@ -93,7 +93,7 @@ export default function AboutPage() {
                                         </p>
                                     </div>
                                 </div>
-                                <div className="bg-cover max-sm:hidden" style={{backgroundImage: "linear-gradient(to right, rgba(231, 231, 231, 1) 0%, rgba(231, 231, 231, 1) 30%, rgba(231, 231, 231, 0.3) 70%, rgba(231, 231, 231, 0.3) 80%, rgba(231, 231, 231, 1) 100%), linear-gradient(to bottom, rgba(231, 231, 231, 1) 0%, rgba(231, 231, 231, 0) 15%, rgba(231, 231, 231, 0) 85%, rgba(231, 231, 231, 1) 100%), url(/stock6big.avif)"}}>
+                                <div className="bg-cover max-sm:hidden" style={{backgroundImage: "linear-gradient(to right, rgba(231, 231, 231, 1) 0%, rgba(231, 231, 231, 1) 30%, rgba(231, 231, 231, 0.3) 70%, rgba(231, 231, 231, 0.3) 80%, rgba(231, 231, 231, 1) 100%), linear-gradient(to bottom, rgba(231, 231, 231, 1) 0%, rgba(231, 231, 231, 0) 15%, rgba(231, 231, 231, 0) 85%, rgba(231, 231, 231, 1) 100%), url(/clarity-path/stock6big.avif)"}}>
                                     <div className="max-md:pb-2 text-left mr-[50%]">
                                         <p className="pb-4 md:text-lg max-md:text-md xl:text-xl">
                                             ADHD, or <b>Attention-Deficit/Hyperactivity Disorder</b>, is a neurodevelopmental condition that affects a person&apos;s ability to focus, control impulses, and manage activity levels appropriately for a given situation. It&apos;s a common disorder, often diagnosed in childhood, but it can persist into adolescence and adulthood.
@@ -128,7 +128,7 @@ export default function AboutPage() {
                             <Tabs variant="fullWidth" theme={tabsTheme}>
                                 <Tabs.Item active title="Diagnostic">
                                     <div className="bg-indigo-500 bg-opacity-70 rounded-lg px-2 py-2 max-w-[110vw]">
-                                        <Image className="w-full h-full rounded-lg border-[2px] border-indigo-300" src="/stock2.avif" alt="diagnostic" width={100} height={100}></Image>
+                                        <Image className="w-full h-full rounded-lg border-[2px] border-indigo-300" src="/clarity-path/stock2.avif" alt="diagnostic" width={100} height={100}></Image>
                                         <div className="pt-2">
                                             <p className="pb-2">Our specialized diagnostic service is designed to provide clarity and support for adults seeking answers about ADHD. Using a compassionate, evidence-based approach, we help you uncover whether ADHD might be impacting your life and offer tailored recommendations for next steps.</p>
                                             <p><b>Your path to clarity starts here.</b></p>
@@ -137,7 +137,7 @@ export default function AboutPage() {
                                 </Tabs.Item>
                                 <Tabs.Item active title="Therapeutic">
                                     <div className="bg-indigo-500 bg-opacity-70 rounded-lg px-2 py-2 max-w-[110vw]">
-                                        <Image className="w-full h-full rounded-lg border-[2px] border-indigo-300" src="/stock3.avif" alt="therapeutic" width={100} height={100}></Image>
+                                        <Image className="w-full h-full rounded-lg border-[2px] border-indigo-300" src="/clarity-path/stock3.avif" alt="therapeutic" width={100} height={100}></Image>
                                         <div className="pt-2">
                                             <p className="pb-2">Through personalized therapy, we help you develop practical strategies to improve focus, organization, and emotional regulation. Our experienced professionals work with you to address specific concerns, whether it&apos;s navigating workplace demands, building stronger relationships, or boosting self-confidence.</p>
                                             <p><b>Let us help you find your path forward.</b></p>
@@ -146,7 +146,7 @@ export default function AboutPage() {
                                 </Tabs.Item>
                                 <Tabs.Item active title="Achievement">
                                     <div className="bg-indigo-500 bg-opacity-70 rounded-lg px-2 py-2 max-w-[110vw]">
-                                        <Image className="w-full h-full rounded-lg border-[2px] border-indigo-300" src="/stock8.avif" alt="achievement" width={100} height={100}></Image>
+                                        <Image className="w-full h-full rounded-lg border-[2px] border-indigo-300" src="/clarity-path/stock8.avif" alt="achievement" width={100} height={100}></Image>
                                         <div className="pt-2">
                                             <p className="pb-2">At Clarity Path, we know that understanding and managing ADHD is just the beginning. Our Achievement Service is designed to help you take the next step—transforming newfound clarity and skills into meaningful progress toward your goals.</p>
                                             <p><b>Step into a future of possibility and achievement with Clarity Path.</b></p>
@@ -164,7 +164,7 @@ export default function AboutPage() {
                                             <p><b>Your path to clarity starts here.</b></p>
                                         </div>
                                         <div className="flex items-center justify-center">
-                                            <Image className="w-full max-w-[36rem] h-auto rounded-lg border-indigo-300 border-[3px]" src="/stock2.avif" alt="diagnostic" width={100} height={100}></Image>
+                                            <Image className="w-full max-w-[36rem] h-auto rounded-lg border-indigo-300 border-[3px]" src="/clarity-path/stock2.avif" alt="diagnostic" width={100} height={100}></Image>
                                         </div>
                                     </div>
                                 </Tabs.Item>
@@ -175,7 +175,7 @@ export default function AboutPage() {
                                             <p><b>Let us help you find your path forward.</b></p>
                                         </div>
                                         <div className="flex items-center justify-center">
-                                            <Image className="w-full max-w-[36rem] h-auto rounded-lg border-indigo-300 border-[3px]" src="/stock3.avif" alt="therapeutic" width={100} height={100}></Image>
+                                            <Image className="w-full max-w-[36rem] h-auto rounded-lg border-indigo-300 border-[3px]" src="/clarity-path/stock3.avif" alt="therapeutic" width={100} height={100}></Image>
                                         </div>
                                     </div>
                                 </Tabs.Item>
@@ -186,7 +186,7 @@ export default function AboutPage() {
                                             <p><b>Step into a future of possibility and achievement with Clarity Path.</b></p>
                                         </div>
                                         <div className="flex items-center justify-center">
-                                            <Image className="w-full max-w-[36rem] h-auto rounded-lg border-indigo-300 border-[3px]" src="/stock8.avif" alt="achievement" width={100} height={100}></Image>
+                                            <Image className="w-full max-w-[36rem] h-auto rounded-lg border-indigo-300 border-[3px]" src="/clarity-path/stock8.avif" alt="achievement" width={100} height={100}></Image>
                                         </div>
                                     </div>
                                 </Tabs.Item>
@@ -217,21 +217,21 @@ export default function AboutPage() {
 
                         <div className="h-[36rem] max-sm:hidden">
                             <Carousel indicators={false} theme={carouselTheme} pauseOnHover={true} slideInterval={6000} className="">
-                                <div className="bg-cover py-24 md:py-48 xl:py-64 px-2 bg-no-repeat w-full h-full bg-center" style={{backgroundImage: `linear-gradient(to right, rgba(231, 231, 231, 1) 0%, rgba(231, 231, 231, 0.2) 60%, rgba(231, 231, 231, 0.2) 85%, rgba(231, 231, 231, 1) 100%), url(/testimonial1${isExtraLarge ? "large" : ""}.avif)`}}>
+                                <div className="bg-cover py-24 md:py-48 xl:py-64 px-2 bg-no-repeat w-full h-full bg-center" style={{backgroundImage: `linear-gradient(to right, rgba(231, 231, 231, 1) 0%, rgba(231, 231, 231, 0.2) 60%, rgba(231, 231, 231, 0.2) 85%, rgba(231, 231, 231, 1) 100%), url(/clarity-path/testimonial1${isExtraLarge ? "large" : ""}.avif)`}}>
                                     <div className="md:text-lg max-md:text-md xl:text-xl mr-[70%]">
                                         <p className="pb-2"><b>Fake Name, 24</b></p>
                                         <p><i>&quot;Clarity Path helped me understand what was really going on in my mind, and allowed me to take a step back so I could tackle each issue head on. Now, months later, I&apos;m achieving things I never could have dreamed of previously.&quot;</i></p>
                                     </div>
                                 </div>
                             
-                                <div className="bg-cover py-24 md:py-48 xl:py-64 px-2 bg-no-repeat w-full h-full" style={{backgroundImage: `linear-gradient(to left, rgba(231, 231, 231, 1) 0%, rgba(231, 231, 231, 0.2) 60%, rgba(231, 231, 231, 0.2) 85%, rgba(231, 231, 231, 1) 100%), url(/testimonial2.avif)`}}>
+                                <div className="bg-cover py-24 md:py-48 xl:py-64 px-2 bg-no-repeat w-full h-full" style={{backgroundImage: `linear-gradient(to left, rgba(231, 231, 231, 1) 0%, rgba(231, 231, 231, 0.2) 60%, rgba(231, 231, 231, 0.2) 85%, rgba(231, 231, 231, 1) 100%), url(/clarity-path/testimonial2.avif)`}}>
                                     <div className="md:text-lg max-md:text-md xl:text-xl ml-[70%] text-right">
                                         <p className="pb-2"><b>This Guy, 29</b></p>
                                         <p><i>&quot;Working with Clarity Path has been a game-changer for me. As an adult with ADHD, I struggled with staying organized and focused, both in my career and personal life. The personalized strategies and support I received have helped me manage my distractions and set achievable goals.&quot;</i></p>
                                     </div>
                                 </div>
 
-                                <div className="bg-cover py-24 md:py-48 xl:py-64 px-2 bg-no-repeat w-full h-full bg-center" style={{backgroundImage: `linear-gradient(to right, rgba(231, 231, 231, 1) 0%, rgba(231, 231, 231, 0.2) 60%, rgba(231, 231, 231, 0.2) 85%, rgba(231, 231, 231, 1) 100%), url(/testimonial3${isExtraLarge ? "large" : ""}.avif)`}}>
+                                <div className="bg-cover py-24 md:py-48 xl:py-64 px-2 bg-no-repeat w-full h-full bg-center" style={{backgroundImage: `linear-gradient(to right, rgba(231, 231, 231, 1) 0%, rgba(231, 231, 231, 0.2) 60%, rgba(231, 231, 231, 0.2) 85%, rgba(231, 231, 231, 1) 100%), url(/clarity-path/testimonial3${isExtraLarge ? "large" : ""}.avif)`}}>
                                     <div className="md:text-lg max-md:text-md xl:text-xl mr-[70%]">
                                         <p className="pb-2"><b>Some One, 32</b></p>
                                         <p><i>&quot;I didn&apos;t realize how much ADHD was affecting my daily life until I found Clarity Path. The practical tools and strategies they&apos;ve provided have helped me improve my time management, reduce overwhelm, and stay focused on what matters most.&quot;</i></p>
@@ -243,21 +243,21 @@ export default function AboutPage() {
                         <div className="h-[32rem] sm:hidden overflow-y-scroll">
                             <Carousel indicators={false} theme={carouselTheme} slide={false} className="">
                                 <div className="px-2 py-2 rounded-lg w-full h-full inline-block bg-gray-500">
-                                    <Image className="border-[2px] border-gray-400 rounded-lg w-full h-max" src="/testimonial1.avif" alt="testimonial1" width={100} height={100}></Image>
+                                    <Image className="border-[2px] border-gray-400 rounded-lg w-full h-max" src="/clarity-path/testimonial1.avif" alt="testimonial1" width={100} height={100}></Image>
                                     <div className="text-md text-offwhite pt-2">
                                         <p className="pb-2"><b>Fake Name, 24</b></p>
                                         <p><i>&quot;Clarity Path helped me understand what was really going on in my mind, and allowed me to take a step back so I could tackle each issue head on. Now, months later, I&apos;m achieving things I never could have dreamed of previously.&quot;</i></p>
                                     </div>
                                 </div>
                                 <div className="px-2 py-2 rounded-lg w-full h-full inline-block bg-gray-500">
-                                    <Image className="border-[2px] border-gray-400 rounded-lg w-full h-max" src="/testimonial2.avif" alt="testimonial2" width={100} height={100}></Image>
+                                    <Image className="border-[2px] border-gray-400 rounded-lg w-full h-max" src="/clarity-path/testimonial2.avif" alt="testimonial2" width={100} height={100}></Image>
                                     <div className="text-md text-offwhite pt-2">
                                         <p className="pb-2"><b>This Guy, 29</b></p>
                                         <p><i>&quot;Working with Clarity Path has been a game-changer for me. As an adult with ADHD, I struggled with staying organized and focused, both in my career and personal life. The personalized strategies and support I received have helped me manage my distractions and set achievable goals.&quot;</i></p>
                                     </div>
                                 </div>
                                 <div className="px-2 py-2 rounded-lg w-full h-full inline-block bg-gray-500">
-                                    <Image className="border-[2px] border-gray-400 rounded-lg w-full h-max" src="/testimonial3.avif" alt="testimonial3" width={100} height={100}></Image>
+                                    <Image className="border-[2px] border-gray-400 rounded-lg w-full h-max" src="/clarity-path/testimonial3.avif" alt="testimonial3" width={100} height={100}></Image>
                                     <div className="text-md text-offwhite pt-2">
                                         <p className="pb-2"><b>Some One, 32</b></p>
                                         <p><i>&quot;I didn&apos;t realize how much ADHD was affecting my daily life until I found Clarity Path. The practical tools and strategies they&apos;ve provided have helped me improve my time management, reduce overwhelm, and stay focused on what matters most.&quot;</i></p>
